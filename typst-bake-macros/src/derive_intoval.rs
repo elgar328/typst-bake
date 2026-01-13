fc3b1d3bdda77f184754cfc401570347f5aecb67
elgar328 <cork_unsold_0k@icloud.com>
2026-01-14 08:39:23 +0900

    Add IntoValue/IntoDict derive macros to remove external dependencies
    Users no longer need to add derive_typst_intoval and typst as
    separate dependencies. The derive macros are now built into
    typst-bake-macros and re-exported from typst-bake.
    
    Based on derive_typst_intoval by KillTheMule (Apache-2.0/MIT).

---
+++ typst-bake-macros/src/derive_intoval.rs
@@ -0,0 +1,70 @@
+// Based on derive_typst_intoval by KillTheMule
+// https://github.com/KillTheMule/derive_typst_intoval
+// Licensed under Apache-2.0 / MIT
+
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{Data, DeriveInput, Fields, Result};
+
+/// IntoValue derive 매크로 구현
+pub fn derive_into_value(item: DeriveInput) -> Result<TokenStream> {
+    let (ty, dictentries) = gather_input(&item)?;
+
+    Ok(quote! {
+        impl ::typst_bake::__internal::typst::foundations::IntoValue for #ty {
+            fn into_value(self) -> ::typst_bake::__internal::typst::foundations::Value {
+                let d = ::typst_bake::__internal::typst::foundations::dict!(
+                    #(#dictentries),*
+                );
+                ::typst_bake::__internal::typst::foundations::Value::Dict(d)
+            }
+        }
+    })
+}
+
+/// IntoDict derive 매크로 구현
+pub fn derive_into_dict(item: DeriveInput) -> Result<TokenStream> {
+    let (ty, dictentries) = gather_input(&item)?;
+
+    Ok(quote! {
+        impl #ty {
+            #[inline]
+            #[must_use]
+            pub fn into_dict(self) -> ::typst_bake::__internal::typst::foundations::Dict {
+                ::typst_bake::__internal::typst::foundations::dict!(
+                    #(#dictentries),*
+                )
+            }
+        }
+    })
+}
+
+fn gather_input(item: &DeriveInput) -> Result<(&syn::Ident, Vec<TokenStream>)> {
+    let ty = &item.ident;
+
+    let Data::Struct(ref data) = &item.data else {
+        return Err(syn::Error::new_spanned(item, "only structs are supported"));
+    };
+
+    let Fields::Named(ref fields) = data.fields else {
+        return Err(syn::Error::new_spanned(
+            &data.fields,
+            "only named fields are supported",
+        ));
+    };
+
+    let dictentries: Vec<_> = fields
+        .named
+        .iter()
+        .map(|field| {
+            let ident = field.ident.as_ref().unwrap();
+            let key = ident.to_string();
+
+            quote! {
+                #key => ::typst_bake::__internal::typst::foundations::IntoValue::into_value(self.#ident)
+            }
+        })
+        .collect();
+
+    Ok((ty, dictentries))
+}
